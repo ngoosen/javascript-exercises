c0789382a780d92b8ff9d532b5d15bfb9ba914bf
Noémie Goosen <noemie.goosen@gmail.com>
2024-09-24 18:49:30 +0200

    feat: add exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+  const returnArray = [];
+
+  array.forEach(element => {
+    if (!elementsToRemove.includes(element)) {
+      returnArray.push(element);
+    }
+  });
+
+  return returnArray;
 };
 
 // Do not edit below this line
