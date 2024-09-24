db8d329cb469d6614d06f2d716425c00e12c1e4d
Noémie Goosen <noemie.goosen@gmail.com>
2024-09-24 20:51:46 +0200

    feat: add exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const oldestStack = [];
+  let oldestStackIndex = 0;
 
+  people.forEach((person, index) => {
+    let countFrom = new Date().getFullYear();
+
+    if (person.yearOfDeath) {
+      countFrom = person.yearOfDeath;
+    }
+
+    const age = countFrom - person.yearOfBirth;
+
+    if (oldestStack.length === 0 || oldestStack[oldestStack.length - 1] < age) {
+      oldestStack.push(age);
+      oldestStackIndex = index;
+    }
+  });
+
+  return people[oldestStackIndex];
 };
 
 // Do not edit below this line
