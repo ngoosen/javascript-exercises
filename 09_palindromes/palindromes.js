28d651d8d775b0e588e00aaa3a20a487444b43c1
Noémie Goosen <noemie.goosen@gmail.com>
2024-09-24 20:25:54 +0200

    feat: add exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  const arrayFromString = string.split("");
 
+  let isPalindrom = true;
+
+  for (let i = 0; i < arrayFromString.length; i++) {
+    if (arrayFromString[i] !== arrayFromString[arrayFromString.length - i - 1]) {
+      isPalindrom = false;
+      break;
+    }
+  }
+
+  return isPalindrom;
 };
 
 // Do not edit below this line
