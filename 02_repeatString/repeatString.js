ae33f0737e5f9d7c4d348204f1d9defd1bfc6fd5
Noémie Goosen <noemie.goosen@gmail.com>
2024-09-24 18:28:59 +0200

    feat: add exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+  if (number < 0) {
+    return "ERROR";
+  }
 
+  let returnString = "";
+
+  for (let i = 0; i < number; i++) {
+    returnString += string;
+  }
+
+  return returnString;
 };
 
 // Do not edit below this line
