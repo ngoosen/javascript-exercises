cef023031d915a99a143b4ad299b23699b42b459
Noémie Goosen <noemie.goosen@gmail.com>
2024-09-24 19:00:02 +0200

    feat: add exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,40 @@
-const sumAll = function() {
+function checkParameters(param1, param2) {
+  let isOk = true;;
 
+  if (param1 < 0 || param2 < 0) {
+    isOk = false;
+  }
+
+  if (param1 % 1 !== 0 || param2 % 1 !== 0) {
+    isOk = false;
+  }
+
+  if (typeof param1 !== "number" || typeof param2 !== "number") {
+    isOk = false;
+  }
+
+  return isOk;
+}
+
+const sumAll = function(start, end) {
+  if (!checkParameters(start, end)) {
+    return "ERROR";
+  }
+
+  if (start > end) {
+    const temp = start;
+    start = end;
+    end = temp;
+  }
+
+  let i= start;
+  let sum = 0;
+
+  while (i <= end) {
+    sum += i++;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
