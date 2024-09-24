fbd112c2e3b919885ce17c2f145e2074d4436450
Noémie Goosen <noemie.goosen@gmail.com>
2024-09-24 20:07:10 +0200

    feat: add exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitTemperature) {
+  return Math.round((fahrenheitTemperature - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusTemperature) {
+  return Math.round(((celsiusTemperature * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
