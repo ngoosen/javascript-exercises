dc7964b946a45ba7639ae96ee1634abf53da7bd1
Noémie Goosen <noemie.goosen@gmail.com>
2024-09-24 20:42:46 +0200

    feat: add exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+  position = parseInt(position);
 
+  if (isNaN(position) || position < 0) return "OOPS";
+  if (position === 0) return 0;
+
+  const fibonacciArray = [1, 1];
+
+  while (fibonacciArray.length < position) {
+    const latest = fibonacciArray[fibonacciArray.length - 1];
+    const previous = fibonacciArray[fibonacciArray.length - 2]
+
+    fibonacciArray.push(latest + previous);
+  }
+
+  return fibonacciArray[position - 1];
 };
 
 // Do not edit below this line
